3a124b14072c178253ff626979c9be49a761d0c6
Moloy <moloy.bitspilani@gmail.com>
2018-04-04 20:29:24 +0530

    removed

--- test_code.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import openravepy
-
-if __name__ == "__main__":
-    env = openravepy.Environment()
-    env.Load('er4u_env.xml')
-    env.SetViewer('qtcoin')
-    robot = env.GetRobots()[0]
